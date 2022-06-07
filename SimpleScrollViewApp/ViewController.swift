1d7a5ff68afe8fff5739d4d76a37b7ea59511cf1
Pritam Dash <pritamdash2015@outlook.com>
2022-06-07 20:36:01 +0530

    Initial Commit

---
+++ SimpleScrollViewApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleScrollViewApp
+//
+//  Created by Pritam Dash on 07/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
